6de234f15ae1fa17dd2136253c286906e2937698
Lucas H <lucashcardoso26@gmail.com>
2023-10-30 16:55:59 -0300

    infra accountContext map done

--- src/JwtStore.Infra/Contexts/AccountContext/Mappings/UserMap.cs
+++ src/JwtStore.Infra/Contexts/AccountContext/Mappings/UserMap.cs
@@ -30,7 +30,7 @@ public class UserMap : IEntityTypeConfiguration<User>
             .OwnsOne(x => x.Verification)
             .Property(x => x.ExpiresAt)
             .HasColumnName("EmailVerificationExpiresAt")
-            .IsRequired(true);
+            .IsRequired(false);
 
         builder.OwnsOne(x => x.Email)
             .OwnsOne(x => x.Verification)
@@ -52,5 +52,18 @@ public class UserMap : IEntityTypeConfiguration<User>
             .HasColumnName("PasswordResetCode")
             .IsRequired();
 
+        builder.HasMany(x => x.Roles)
+            .WithMany(x => x.Users)
+            .UsingEntity<Dictionary<string, object>>(
+            "UserRole", role => role
+            .HasOne<Role>()
+            .WithMany()
+            .HasForeignKey("RoleId")
+            .OnDelete(DeleteBehavior.Cascade),
+            user => user
+            .HasOne<User>()
+            .WithMany()
+            .HasForeignKey("UserId")
+            .OnDelete(DeleteBehavior.Cascade));
     }
 }
